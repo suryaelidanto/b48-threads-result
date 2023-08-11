708920f0afba63e7056f46085b20329d36904dd6
suryaa6666 <suryaelidanto@gmail.com>
2023-08-11 07:53:59 +0700

    feat : register and login pages

--- threads-fe/src/pages/Login.tsx
+++ threads-fe/src/pages/Login.tsx
@@ -1,9 +1,21 @@
+import { FormLogin } from "@/features/auth";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Login() {
-
   return (
-    <>
-      
-    </>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      marginTop={"50px"}
+    >
+      <FormLogin />
+      <Box display={"flex"} gap={2}>
+        <Text>Don't have an account yet?</Text>
+        <Text color={"brand.green"} cursor={"pointer"}>
+          Create account
+        </Text>
+      </Box>
+    </Box>
   );
 }
--- threads-fe/src/pages/Register.tsx
+++ threads-fe/src/pages/Register.tsx
@@ -1,9 +1,21 @@
+import { FormRegister } from "@/features/auth";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Register() {
-
   return (
-    <>
-      
-    </>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      marginTop={"50px"}
+    >
+      <FormRegister />
+      <Box display={"flex"} gap={2}>
+        <Text>Already have account?</Text>
+        <Text color={"brand.green"} cursor={"pointer"}>
+          Login
+        </Text>
+      </Box>
+    </Box>
   );
 }
